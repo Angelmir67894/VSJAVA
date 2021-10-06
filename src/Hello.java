ee71830de7b5bd6b8dfbc129633698dd269df014
Angelmir67894 <92046199+Angelmir67894@users.noreply.github.com>
2021-10-06 12:46:17 -0500

    Added Hello Moon

--- src/Hello.java
+++ src/Hello.java
@@ -2,7 +2,9 @@ public class Hello {
     public static void main(String[] args) {
         
         String hello = "Hello World!";
+        String helloM = "Hello Moon!";
 
         System.out.println(hello);
+        System.out.println(helloM);
     }
 }
